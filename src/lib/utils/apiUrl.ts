e86f22fbe20fedc7e49c22f030e118eac625c970
Kevin Midboe <kevin.midboe@gmail.com>
2022-12-29 23:07:52 +0100

    apiUrl builds url to planetposen-backend which prefixes localhost if dev

--- src/lib/utils/apiUrl.ts
+++ src/lib/utils/apiUrl.ts
@@ -5,8 +5,9 @@ const LOCALHOST_API = 'http://localhost:30010';
 export function buildApiUrl(path: string) {
   let localhostApi = false;
 
-  if (dev) localhostApi = true;
-  if (browser && window?.location?.href.includes('localhost')) {
+  if (dev) {
+    localhostApi = true;
+  } else if (browser && window?.location?.href.includes('localhost')) {
     localhostApi = true;
   }
 
