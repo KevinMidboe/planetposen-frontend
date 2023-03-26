8bb65dfc2e535ae27f5668f57ba0518178ead681
Kevin <Kevin.Midboe@gmail.com>
2023-03-26 01:59:23 +0100

    Feat: Checkout page redesign (#6)
    * Increased padding on input elements
    
    * Increased max layout width from 1500 to 1800
    
    * Address input should take 2 column of space
    
    * Updated order section with new design
    
    * Checkout gets a new layout with sidebar
    
    * Express checkout section
    
    * Increase font size at larger screen width
    
    * Environment example file
    
    * Resolved linting issues

---
+++ src/routes/checkout/ExpressSection.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import Vipps from '$lib/icons/Vipps.svelte';
+  import ShopPay from '$lib/icons/ShopPay.svelte';
+  import ApplePay from '$lib/icons/ApplePay.svelte';
+  import PayPal from '$lib/icons/PayPal.svelte';
+  import GooglePay from '$lib/icons/GooglePay.svelte';
+</script>
+
+<ul>
+  <li class="button" tabindex="0"><Vipps /></li>
+  <li class="button" tabindex="0"><ApplePay /></li>
+  <li class="button" tabindex="0"><PayPal /></li>
+  <li class="button" tabindex="0"><GooglePay /></li>
+</ul>
+
+<style lang="scss">
+  ul {
+    width: 100%;
+    display: flex;
+    padding-left: 0;
+    list-style: none;
+
+    @media (max-width: 750px) {
+      flex-wrap: wrap;
+      justify-content: space-around;
+    }
+  }
+
+  .button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0.5rem;
+    width: 8em;
+    height: 42px;
+    border-radius: 4px;
+    background-color: black;
+    cursor: pointer;
+    transition: all 0.3s ease;
+
+    &:hover {
+      scale: 1.05;
+    }
+
+    &:nth-of-type(1) {
+      background-color: #ff5b24;
+    }
+
+    &:nth-of-type(3) {
+      background-color: #ffc439;
+    }
+
+    @media (max-width: 999px) {
+      width: 10em;
+      margin: 0.5rem 0.2rem;
+    }
+  }
+</style>
--- src/routes/checkout/OrderSection.svelte
+++ src/routes/checkout/OrderSection.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import OrderTotalSection from './OrderTotalSection.svelte';
   import QuantitySelect from '$lib/components/QuantitySelect.svelte';
 
   import { cart, subTotal } from '$lib/cartStore';
@@ -12,130 +13,201 @@
   }
 </script>
 
-<div style="border: 2px solid black">
-  <table class="order-summary">
-    <thead style="border-bottom: 2px solid black;">
+<div class="order-summary">
+  <table class="product-table">
+    <caption class="visually-hidden"> Shopping cart </caption>
+    <thead class="product-table__header">
       <tr>
-        <th>Varenavn</th>
-        <th>Antall</th>
-        <th>Pris</th>
+        <th scope="col"><span class="visually-hidden">Product image</span></th>
+        <th scope="col"><span class="visually-hidden">Description</span></th>
+        <th scope="col"><span class="visually-hidden">Quantity</span></th>
+        <th scope="col"><span class="visually-hidden">Price</span></th>
       </tr>
     </thead>
 
-    <tbody>
-      {#if $cart.length}
-        {#each $cart as cartItem}
-          <tr id="{lineItemClass(cartItem.lineitem_id)}">
-            <td>
-              <div class="line-order">
-                <a href="/shop/{cartItem.product_no}"><span>{cartItem.name}</span></a>
-                <span class="subtext">Størrelse: {cartItem.size}</span>
+    <tbody data-order-summary-section="line-items">
+      {#each $cart as cartItem}
+        <tr
+          class="product"
+          data-product-id="6718367989809"
+          data-variant-id="39715003924529"
+          data-product-type=""
+          data-customer-ready-visible=""
+        >
+          <td class="product__image">
+            <div class="product-thumbnail">
+              <div class="product-thumbnail__wrapper">
+                <img
+                  alt="Black Googly Eye Puff Print Logo Tee - XS"
+                  class="product-thumbnail__image"
+                  src="{cartItem.image}"
+                  data-src="//cdn.shopify.com/s/files/1/0023/3789/8540/products/20220718_A24_GooglyEye_Tee_Black_15991x1gray_small.jpg?v=1659020903"
+                />
               </div>
-            </td>
-            <td>
-              <QuantitySelect
-                bind:value="{cartItem.quantity}"
-                hideButtons="{true}"
-                on:decrement="{() => decrementProductInCart(cartItem.lineitem_id)}"
-                on:increment="{() => incrementProductInCart(cartItem.lineitem_id)}"
-              />
-            </td>
-            <td>Nok {cartItem.quantity * cartItem.price}</td>
-          </tr>
-        {/each}
-      {:else}
-        <tr class="no-products">
-          <td>(ingen produkter)</td>
-          <td>0</td>
-          <td>Nok 0</td>
+              <span class="product-thumbnail__quantity" aria-hidden="true">{cartItem.quantity}</span
+              >
+            </div>
+          </td>
+          <th class="product__description" scope="row">
+            <span class="product__description__name order-summary__emphasis">{cartItem.name}</span>
+            <span class="product__description__variant order-summary__small-text"
+              >{cartItem.size}</span
+            >
+          </th>
+          <td class="product__quantity">
+            <span class="visually-hidden"> 1 </span>
+          </td>
+          <td class="product__price">
+            <p class="order-summary__emphasis skeleton-while-loading">
+              NOK {cartItem.quantity * cartItem.price}
+            </p>
+          </td>
         </tr>
-      {/if}
-
-      <tr style="border-bottom-color: rgba(0,0,0,0.15)">
-        <td>Totalsum:</td>
-        <td></td>
-        <td>Nok {$subTotal}</td>
-      </tr>
-
-      <tr style="border-bottom-color: rgba(0,0,0,0.15)">
-        <td>Frakt:</td>
-        <td></td>
-        <td>Nok {shippingPrice}</td>
-      </tr>
-
-      <tr style="font-weight: 600">
-        <td>Pris:</td>
-        <td></td>
-        <td>Nok {totalPrice}</td>
-      </tr>
+        <tr class="spacer"></tr>
+      {/each}
     </tbody>
   </table>
+
+  <OrderTotalSection />
 </div>
 
-<!-- <slot name="express-checkout-buttons" /> -->
 <style lang="scss" module="scoped">
-  table.order-summary {
-    width: 100%;
+  table,
+  tr,
+  td,
+  th {
     border-collapse: collapse;
+    border-spacing: 0;
+  }
 
-    thead {
-      th {
-        padding: 1rem 0.5rem;
-        min-width: 50px;
-        font-size: 1.3rem;
+  .order-summary {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+
+  :global(.visually-hidden) {
+    border: 0;
+    clip: rect(0, 0, 0, 0);
+    clip: rect(0 0 0 0);
+    width: 2px;
+    height: 2px;
+    margin: -2px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    white-space: nowrap;
+  }
+
+  :global(.hidden) {
+    display: none;
+  }
+
+  .product-table {
+    margin-top: 0.75em;
+    margin-bottom: 1.5em;
+    position: relative;
+    z-index: 1;
+
+    &__header th {
+      padding-top: 0;
+      padding-bottom: 0;
+      white-space: nowrap;
+      font-weight: 500;
+      color: #333333;
+    }
+
+    .spacer {
+      height: 0.7rem;
+    }
+
+    .product {
+      &__image {
+        .product-thumbnail {
+          width: 4.6em;
+          height: 4.6em;
+          border-radius: 8px;
+          background: #fff;
+          position: relative;
+
+          &__wrapper {
+            width: 100%;
+            height: 100%;
+            position: relative;
+            overflow: hidden;
+            border-radius: 8px;
+            z-index: 1;
+          }
+
+          &__image {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            max-width: 100%;
+            max-height: 100%;
+            width: 100%;
+            margin: auto;
+          }
+
+          &__quantity {
+            font-size: 0.8571428571em;
+            font-weight: 500;
+            line-height: 1.75em;
+            white-space: nowrap;
+            text-align: center;
+            border-radius: 1.75em;
+            background-color: rgba(114, 114, 114, 0.9);
+            background-color: rgb(24, 51, 47, 0.9);
+            color: #fff;
+            -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+            min-width: 1.75em;
+            height: 1.75em;
+            padding: 0 0.5833333333em;
+            position: absolute;
+            right: -0.75em;
+            top: -0.75em;
+            z-index: 3;
+          }
+        }
+      }
+
+      &__quantity {
+      }
+
+      &__description {
         text-align: left;
-        font-weight: 500;
-      }
-    }
+        width: 100%;
+        padding-top: 0.8rem;
+        vertical-align: top;
+        padding-bottom: 0;
+        padding-left: 1rem;
 
-    th:nth-of-type(2),
-    td:nth-of-type(2) {
-      width: 10%;
-    }
+        &__name {
+          display: block;
+          color: #787878;
+          width: 1em;
+          min-width: 100%;
+          font-weight: 500;
+        }
 
-    th:last-of-type,
-    td:last-of-type {
-      width: 30%;
-      padding-left: 1rem;
-    }
-
-    tr.no-products {
-      color: rgba(0, 0, 0, 0.5);
-    }
-
-    tr:not(:last-of-type) {
-      border-bottom: 2px solid black;
-    }
-
-    td {
-      padding: 1rem 0.5rem;
-      min-width: 50px;
-      font-size: 1.1rem;
-      text-align: left;
-
-      &:nth-of-type(2) {
-        border-left: none;
-        border-right: none;
+        &__variant {
+          display: block;
+          font-size: 0.85rem;
+          color: #6e6e6e;
+        }
       }
 
-      &:first-of-type {
-        border-right: none;
-      }
-
-      &:last-of-type {
-        border-left: none;
-      }
-    }
-
-    .line-order {
-      padding: 9px 12px;
-      font-weight: 400;
-      text-align: left;
-      vertical-align: top;
-
-      .subtext {
-        color: #999;
-        display: block;
+      &__price {
+        font-size: 0.875rem;
+        line-height: 1.2;
+        vertical-align: top;
+        text-align: right;
+        padding-top: 0.69rem;
+        padding-right: 0.5rem;
+        white-space: nowrap;
       }
     }
   }
