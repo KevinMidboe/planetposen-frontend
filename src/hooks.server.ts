b56be97f86218f022e935a7be9540d284023c87a
KevinMidboe <kevin.midboe@gmail.com>
2024-03-03 19:19:46 +0100

    Use env var API_HOST to direct where backend lives.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,13 @@
+import { env } from '$env/dynamic/private';
 import type { HandleFetch } from '@sveltejs/kit';
 
 export const handleFetch: HandleFetch = async ({ request, fetch }) => {
   const { origin } = new URL(request.url);
+  const host = env?.API_HOST || 'http://localhost:30010';
 
   if (request.url.startsWith(`${origin}/api`)) {
     // clone the original request, but change the URL
-    request = new Request(request.url.replace(origin, 'http://localhost:30010'), request);
+    request = new Request(request.url.replace(origin, host), request);
   }
 
   return fetch(request);
